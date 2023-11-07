42aa6950b0c0ef95d859f599408e5e6619e872c9
0DirtySun1 <bonbonrinsaal@web.de>
2023-11-07 12:12:07 +0100

    added an option to choose which type of password you want to generate

--- #password-generator.py
+++ #password-generator.py
@@ -3,15 +3,19 @@
 def main():
     import random
     import string
-    import pyperclip
-    #Get user input for password length
+    import pyperclip    
+
     length = int(input("How long do you want your password to be? "))
+
     while True:
-        content = str(input("What do you want your password to contain(letters, numbers, all)? "))
+        content = str(input("What do you want your password to contain(letters, numbers, numbers+letters, all)? "))
         if content == "letters":
             characters = string.ascii_letters
             break
         elif content == "numbers":
+            characters = string.digits
+            break
+        elif content == "numbers+letters":
             characters = string.ascii_letters + string.digits
             break
         elif content == "all":
@@ -19,12 +23,10 @@ def main():
             break
         else:
             print("Please enter a valid input")
-    #Create a password variable
     password = ""
-    #Create a loop to generate a random password
     for i in range(length):
         password += random.choice(characters)
-    #Print the password
+
     print("This is your password: " + password)
     pyperclip.copy(password)
 main()
